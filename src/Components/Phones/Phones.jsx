c2b74e479a10bf1e2c40b84f027ee3fd05d94031
nayan-28 <nayanmalakar720@gmail.com>
2024-02-07 00:43:34 +0600

    completed

--- src/Components/Phones/Phones.jsx
+++ src/Components/Phones/Phones.jsx
@@ -12,8 +12,10 @@ import {
   ResponsiveContainer,
 } from "recharts";
 
+import { Audio, Grid } from "react-loader-spinner";
 const Phones = () => {
   const [phones, setPhones] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     // fetch(" https://openapi.programming-hero.com/api/phones?search=iphone")
@@ -30,12 +32,35 @@ const Phones = () => {
           };
           return obj;
         });
-        console.log(phonesWithFakeData);
         setPhones(phonesWithFakeData);
+        setLoading(false);
       });
   }, []);
   return (
     <div>
+      {loading && (
+        <div>
+          <Audio
+            height="80"
+            width="80"
+            radius="9"
+            color="green"
+            ariaLabel="thee-dots-loading"
+            wrapperStyle
+            wrapperClass
+          />
+          <Grid
+            height="80"
+            width="80"
+            radius="12.5"
+            color="#4fa94d"
+            ariaLabel="grid-loading"
+            wrapperStyle={{}}
+            wrapperClass=""
+            visible={true}
+          />
+        </div>
+      )}
       <h2 className="text-5xl">Phones:{phones.length}</h2>
       <BarChart width={600} height={400} data={phones}>
         <Bar dataKey="price" fill="#8884d8" />
